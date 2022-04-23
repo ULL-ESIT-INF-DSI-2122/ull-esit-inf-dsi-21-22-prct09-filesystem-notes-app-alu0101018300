3b00dd1863f79a891a1fb5efbb6c931dfc050cad
alu0101018300 <alu0101018300@ull.edu.es>
2022-04-23 17:48:30 +0000

    Método readNote completado

--- src/notesManager.ts
+++ src/notesManager.ts
@@ -6,13 +6,34 @@ export class NotesManager implements noteManagement{
   private _path: string = '';
   constructor() {}
 
-  private establishPath(user:string):void {
+  private establishPath(user: string): void {
     this._path = `/home/usuario/Practicas/Practica9/ull-esit-inf-dsi-21-22-prct09-filesystem-notes-app-alu0101018300/notes/${user}`;
   }
 
   private addFolder() {
     if (!fs.existsSync(this._path)) {
       fs.mkdirSync(this._path);
+    }
+  }
+
+  private travelNotes(path: string) {
+    const note = JSON.parse(fs.readFileSync(path, 'utf8'));
+    return note;
+  }
+
+  private print(color: string, body:string) {
+    if (color === 'red') {
+      console.log(chalk.red(body));
+    } else if (color === 'green') {
+      console.log(chalk.green(body));
+    } else if (color === 'blue') {
+      console.log(chalk.blue(body));
+    } else if (color === 'yellow') {
+      console.log(chalk.yellow(body));
+    }
+    else {
+      console.log(body);
+      console.log(chalk.red.inverse(`${color} is not available`));
     }
   }
 
@@ -30,6 +51,23 @@ export class NotesManager implements noteManagement{
       return true;
     } else {
       console.log(chalk.red.inverse(`The Note Already Exists.`));
+      return false;
+    }
+  }
+
+  public readNote(user: string, title: string): boolean {
+    this.establishPath(user);
+    const notePath:string = this._path + '/' + title + '.json';
+    if (fs.existsSync(notePath)) {
+      const note = this.travelNotes(notePath);
+      const body = note.body;
+      const color = note.color;
+      console.log(chalk.green(`Note ${note.title}: `));
+      this.print(color, body);
+      return true;
+    } 
+    else {
+      console.log(chalk.red('The note does not exist.'));
       return false;
     }
   }
